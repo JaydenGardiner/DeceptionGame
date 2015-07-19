7f73dd8b2e16a830f6b32f19039b92bdd6b78fe9
David <dibarbet@gmail.com>
2015-07-19 19:15:36 -0400

    Initial setup for board
    Created sprite prefabs

---
+++ DeceptionGame/Assets/PieceMover.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(BoxCollider2D))]
+public class PieceMover : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        Debug.Log("INIT NUMBER SCRIPT");
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+
+    }
+
+// Attach this script to an orthographic camera.
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Debug.Log("RUNNING MOUSE DRAG");
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+}
