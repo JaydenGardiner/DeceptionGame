1c303ffc8061e399093a1ac20a81a99f1926b43d
David <dibarbet@gmail.com>
2015-09-14 15:36:02 -0400

    Imported piece + camera movement
    Imported piece, camera moves on mouse movement around Vector3.up

--- Not Trespass/Assets/Scripts/BoardManager.cs
+++ Not Trespass/Assets/Scripts/BoardManager.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class BoardManager : MonoBehaviour {
 
+    public GameObject board;
+
 	// Use this for initialization
 	void Start () {
-	
+        board = this.gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Debug.Log(this.transform.position);
 	}
 }
---
+++ Not Trespass/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    public GameObject board;
+
+    private float speed = 5f;
+    private Vector3 target;
+	// Use this for initialization
+	void Start () {
+        //Renderer render = board.GetComponent<Renderer>();
+        //target = render.bounds.center;
+        target = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    this.transform.LookAt(target);
+        //this.transform.Translate(Vector3.right * Time.deltaTime * speed);
+        this.transform.RotateAround(target, Vector3.up, Input.GetAxis("Mouse X") * speed);
+	}
+}
