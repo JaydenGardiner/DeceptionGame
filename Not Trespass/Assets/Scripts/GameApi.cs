397e38885c2c16160bbb6d68e0e5ff81bef0cb15
Thomas Shields <thomas.shields@gatech.edu>
2015-10-06 04:10:23 -0400

    Added some unit tests, wip connecting api into mono

--- Not Trespass/Assets/Scripts/GameApi.cs
+++ Not Trespass/Assets/Scripts/GameApi.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Net;
 
 public class RemoteApi {
+	private const String API_BASE = "http://localhost:5000";
+	private  const String GAME_RES = "game";
+
     private RemoteApi() { }
     public static RemoteApi getInstance(String user, String password) {
         return new RemoteApi();
@@ -8,12 +12,14 @@ public class RemoteApi {
 
     
     public void updateGameState(int gameId, Piece[,] pieces) {
-    
+	
     }
 
     public Piece[,] getGameState(int gameId) {
-        return null;
+		WebClient wc = new WebClient ();
+		Console.WriteLine (wc.DownloadString (String.Join ("/", new string[] { API_BASE, GAME_RES, "" + gameId })));
+       
+		return null;
     }
-
-    
+   
 }
