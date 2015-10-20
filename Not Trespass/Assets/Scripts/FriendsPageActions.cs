94e0a24ff3f1e4e7f44c0b22ca3162db25959c16
Pranav <ppranavv5@gmail.com>
2015-10-19 23:43:13 -0400

    add and remove friend functionality is there

---
+++ Not Trespass/Assets/Scripts/FriendsPageActions.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FriendsPageActions : MonoBehaviour {
+
+    public ArrayList friends;
+    public InputField EmailInput;
+    public Text friend;
+    
+
+	// Use this for initialization
+	public void Start () {
+        friends = new ArrayList();
+        //        EmailInput.contentType = InputField.ContentType.EmailAddress;\
+    }
+	
+	// Update is called once per frame
+	public void Update () {
+	
+	}
+
+    public void addFriend()
+    {
+        if (EmailInput.text != "")
+        {
+            friends.Add(EmailInput.text);
+            friend.text = EmailInput.text;
+            EmailInput.text = "";
+            
+        }
+    }
+
+        public void removeFriend()
+    {
+        friends.Remove(EmailInput.text);
+        friend.text = "";
+    }
+
+}
