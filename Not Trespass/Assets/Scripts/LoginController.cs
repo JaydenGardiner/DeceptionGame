fe8f8341d2680c8431a2b615e4b2525c8d56f1c3
Pranav <ppranavv5@gmail.com>
2015-11-08 20:49:39 -0500

    added a login screen

---
+++ Not Trespass/Assets/Scripts/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginController : MonoBehaviour {
+
+    private GameObject loginCanvas;
+    // Use this for initialization
+    void Start () {
+        loginCanvas = GameObject.Find("Canvas");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+}
